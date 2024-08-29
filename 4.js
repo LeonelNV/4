ab57070935591cf1621bb7a230320be4f1ab0056
Humbucker <90539813+LeonelNV@users.noreply.github.com>
2024-08-28 22:55:04 -0600

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,6 @@
+ //4 - Ordenamiento de un arreglo
+ function ordenarArreglo(numeros){
+    return arreglo = numeros.sorft((a,b)=>a-b);
+ }
+ let numeros = [4, 5, 7, 1, 3];
+ console.log("Arreglo ordenado: ", ordenarArreglo(numeros));
